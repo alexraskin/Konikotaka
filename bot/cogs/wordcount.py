df958da811e2cf7871fcf3e55b521e004c66ae5d
alexraskin <alexraskin@fastmail.fm>
2023-09-05 13:46:12 -0700

    add word count

---
+++ bot/cogs/wordcount.py
@@ -0,0 +1,71 @@
+from discord.ext import commands, tasks
+
+from models.word_count import WordCount
+from models.db import Base
+
+class WordCounter(commands.Cog, name="Word Count"):
+    def __init__(self, client) -> None:
+        self.client = client
+        Base.metadata.create_all(self.client.engine, checkfirst=True)
+        self.build_word_list.start()
+    
+    @tasks.loop(seconds=10)
+    async def build_word_list(self):
+        self.word_list = (
+            self.client.db_session.query(WordCount)
+            .order_by(WordCount.count.desc())
+            .all()
+        )
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        for word in self.word_list:
+            if word.word in message.content.lower():
+                word.count += 1
+                await message.channel.send(f"Word `{word.word}` has been said {word.count} times!")
+                self.client.db_session.commit()
+    
+    @commands.command(name="addword", description="Add a new word to track the count of")
+    async def add_word(self, ctx: commands.Context, word: str):
+        if len(word) > 255:
+            return await ctx.send("Word is too long.")
+        try:
+            new_word = WordCount(
+                word=word.strip(),
+                discord_id=ctx.author.id,
+            )
+            self.client.db_session.add(new_word)
+            self.client.db_session.commit()
+            await ctx.send(f"Word `{word}` added!")
+            await ctx.message.add_reaction("👍")
+        except Exception as e:
+            print(e)
+            self.client.db_session.rollback()
+            await ctx.send("An error occurred while adding the word.", ephemeral=True)
+    
+    @commands.command(name="removeword", description="Remove a word from being tracked")
+    async def remove_word(self, ctx: commands.Context, word: str):
+        if len(word) > 255:
+            return await ctx.send("Word is too long.")
+        try:
+            word = (
+                self.client.db_session.query(WordCount)
+                .filter(WordCount.word == word)
+                .first()
+            )
+            if word:
+                self.client.db_session.delete(word)
+                self.client.db_session.commit()
+                await ctx.send(f"Word `{word}` removed!")
+                await ctx.message.add_reaction("👍")
+            else:
+                await ctx.send(f"Word `{word}` not found.")
+        except Exception as e:
+            print(e)
+            self.client.db_session.rollback()
+            await ctx.send("An error occurred while removing the word.", ephemeral=True)
+
+async def setup(client):
+    await client.add_cog(WordCounter(client))
---
+++ bot/models/word_count.py
@@ -0,0 +1,9 @@
+from models.db import Base
+from sqlalchemy import Column, Integer, VARCHAR, BIGINT
+
+class WordCount(Base):
+    __tablename__ = "word_count"
+    id = Column(Integer, primary_key=True)
+    word = Column(VARCHAR(255), nullable=False)
+    count = Column(BIGINT, nullable=False, default=0)
+    discord_id = Column(VARCHAR(25), nullable=False)
