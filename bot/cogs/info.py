9316d4fd5cc5de08a60ccb59511311e39cc8fa39
alexraskin <alexraskin@fastmail.fm>
2023-10-22 01:21:13 -0700

    lint

--- bot/cogs/info.py
+++ bot/cogs/info.py
@@ -26,7 +26,7 @@ class Info(commands.Cog, name="Info"):
         description = str(
             "My personal bot, provides some useful commands and some fun commands. "
             "The name Konikotaka comes from the TV Show - [The Office](https://youtu.be/Qr2LQILdXD0)"
-          )
+        )
 
         embed = Embed(
             description=description,
