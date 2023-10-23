0676cb3879f51e76572fc90fed579ba8f33b436d
alexraskin <alexraskin@fastmail.fm>
2023-10-22 20:12:22 -0700

    update invite command

--- bot/cogs/info.py
+++ bot/cogs/info.py
@@ -4,16 +4,15 @@ import os
 import platform
 
 import pkg_resources
-from discord import Colour, Embed, Member, app_commands
+from discord import Colour, Embed, Member, app_commands, Permissions
 from discord.ext import commands
-
+from discord.utils import oauth_url
 from .utils.utils import date
 
 
 class Info(commands.Cog, name="Info"):
     def __init__(self, client: commands.Bot) -> None:
         self.client: commands.Bot = client
-        self.invite_url = "https://discord.com/api/oauth2/authorize?client_id=482960108117295105&permissions=10415295163479&scope=bot"
 
     @commands.hybrid_command(
         name="info", help="Get info about the bot", with_app_command=True
@@ -33,7 +32,7 @@ class Info(commands.Cog, name="Info"):
             timestamp=ctx.message.created_at,
         )
         embed.title = "Konikotaka"
-        embed.url = self.invite_url
+        embed.url = "https://konikotaka.dev/"
         embed.colour = Colour.blurple()
         embed.set_author(
             name=str(self.client.owner), icon_url=self.client.owner.display_avatar.url
@@ -55,10 +54,27 @@ class Info(commands.Cog, name="Info"):
         embed.set_thumbnail(url=self.client.user.display_avatar.url)
         await ctx.send(embed=embed)
 
-    @commands.hybrid_command("invite", with_app_command=True)
-    async def invite(self, ctx: commands.Context) -> None:
-        """Invite me to your server!"""
-        await ctx.send(self.invite_url)
+    @commands.hybrid_command("join", with_app_command=True)
+    async def join(self, ctx: commands.Context):
+        """Posts my invite to allow you to invite me"""
+        perms = Permissions.none()
+        perms.read_messages = True
+        perms.external_emojis = True
+        perms.send_messages = True
+        perms.manage_roles = True
+        perms.manage_channels = True
+        perms.ban_members = True
+        perms.kick_members = True
+        perms.manage_messages = True
+        perms.manage_expressions = True
+        perms.embed_links = True
+        perms.speak = True
+        perms.connect = True
+        perms.read_message_history = True
+        perms.attach_files = True
+        perms.add_reactions = True
+        perms.use_application_commands = True
+        await ctx.send(f'<{oauth_url(self.bot.client_id, permissions=perms)}>')
 
     @commands.hybrid_command(name="user", aliases=["member"])
     @commands.guild_only()
