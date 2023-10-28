d362697630caef8c3573f8afa181c6c430fc1ee9
Alex <alexraskin@fastmail.fm>
2023-10-28 11:18:39 -0700

    Update info.py

--- bot/cogs/info.py
+++ bot/cogs/info.py
@@ -25,7 +25,7 @@ class Info(commands.Cog, name="Info"):
 
         description = str(
             "My personal bot, provides some useful and fun commands. "
-            "The name Konikotaka comes from the TV Show - [The Office](https://youtu.be/Qr2LQILdXD0)"
+            "The name **Konikotaka** comes from the [The Office](https://cdn.theoffice.foo/Dwight-Schrutes-Perfect-Girl-Speech-Anime.mp4)"
         )
 
         embed = Embed(
