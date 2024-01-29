b285c764de4a1ed4c6d9c973c0d92229e3c0b93f
alexraskin <alexraskin@fastmail.fm>
2024-01-28 23:00:18 -0700

    lint fix

--- bot/cogs/rcon.py
+++ bot/cogs/rcon.py
@@ -18,7 +18,9 @@ class Rcon(commands.Cog, name="Rcon"):
         )
         self.steam_profile = "https://steamcommunity.com/profiles/{steam_id}/"
 
-    @app_commands.command(name="rcon_commands", description="Get the list of rcon commands")
+    @app_commands.command(
+        name="rcon_commands", description="Get the list of rcon commands"
+    )
     async def rcon_commands(self, interaction: Interaction) -> None:
         embed = Embed(title="Rcon Commands", colour=Colour.blurple())
         embed.add_field(name="Info", value="Get the server info")
@@ -26,29 +28,39 @@ class Rcon(commands.Cog, name="Rcon"):
         embed.add_field(name="ShowPlayers", value="Get the list of players online")
         embed.add_field(name="Broadcast", value="Broadcast a message to the server")
         await interaction.response.send_message(embed=embed)
-    
+
     @app_commands.command(name="online", description="Get the list of players online")
     async def online(self, interaction: Interaction) -> None:
-      output, players = self.rcon_client.online()
-      if not output:
-          await interaction.response.send_message("There was an error getting the list of players online.")
-          return
-      embed = Embed(title="Players Online", colour=Colour.blurple(), description=f"Player(s) Online: {len(players)}")
-      list_of_players = ""
-      for player in players:
-          list_of_players += f"[{player[0]}]({self.steam_profile.format(steam_id=player[1])})\n"
-      embed.add_field(name="Players", value=list_of_players, inline=False)
-      await interaction.response.send_message(embed=embed)
-         
+        output, players = self.rcon_client.online()
+        if not output:
+            await interaction.response.send_message(
+                "There was an error getting the list of players online."
+            )
+            return
+        embed = Embed(
+            title="Players Online",
+            colour=Colour.blurple(),
+            description=f"Player(s) Online: {len(players)}",
+        )
+        list_of_players = ""
+        for player in players:
+            list_of_players += (
+                f"[{player[0]}]({self.steam_profile.format(steam_id=player[1])})\n"
+            )
+        embed.add_field(name="Players", value=list_of_players, inline=False)
+        await interaction.response.send_message(embed=embed)
+
     @app_commands.command(name="rcon_info", description="Get the server info")
     async def rcon_info(self, interaction: Interaction) -> None:
-          info = self.rcon_client.info()
-          if not info:
-              await interaction.response.send_message("There was an error getting the server info.")
-              return
-          embed = Embed(title="Server Info", colour=Colour.blurple(), description=info)
-          await interaction.response.send_message(embed=embed)
+        info = self.rcon_client.info()
+        if not info:
+            await interaction.response.send_message(
+                "There was an error getting the server info."
+            )
+            return
+        embed = Embed(title="Server Info", colour=Colour.blurple(), description=info)
+        await interaction.response.send_message(embed=embed)
 
-  
+
 async def setup(client: commands.Bot) -> None:
     await client.add_cog(Rcon(client))
